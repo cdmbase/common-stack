d52cedf2670ffbbd655fb5cffc25dc8eaac0a418
veeramarni <mvasireddy@cdmbase.com>
2018-02-24 15:16:34 -0500

    update redux package

---
+++ packages/common-client-redux/src/impl/reducer-registry.ts
@@ -0,0 +1,29 @@
+import { IReducerRegistry } from '../interfaces';
+import { ReducersMapObject, AnyAction, Reducer } from 'redux';
+
+class ReducerRegistry implements IReducerRegistry<any> {
+    private _emitChange;
+    private _reducers: ReducersMapObject;
+
+    constructor() {
+        this._emitChange = null;
+        this._reducers = {};
+    }
+
+    public getReducers() {
+        return { ...this._reducers };
+    }
+
+    public register(name, reducer) {
+        this._reducers = { ...this._reducers, [name]: reducer };
+        if (this._emitChange) {
+            this._emitChange(this.getReducers());
+        }
+    }
+
+    public setChangeListener(listener) {
+        this._emitChange = listener;
+    }
+}
+
+export { ReducerRegistry };

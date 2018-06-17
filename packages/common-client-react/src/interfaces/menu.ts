3b82228796587fcd0bc306c19989dd74afe28ab4
veeramarni <mvasireddy@cdmbase.com>
2018-06-17 11:12:08 -0400

    added menu position

--- packages/common-client-react/src/interfaces/menu.ts
+++ packages/common-client-react/src/interfaces/menu.ts
@@ -3,6 +3,7 @@
 export interface IMenuData {
     [key: string]: {
         name: any;
+        position?: IMenuPosition;
         icon?: any;
         authority?: any;
         exact?: boolean;
@@ -15,9 +16,18 @@ export interface IMenuItem {
     path?: string;
     name: any;
     icon?: any;
+    position: IMenuPosition;
     authority?: any;
     loading?: any;
     exact?: boolean;
     strict?: boolean;
     children?: IMenuItem[];
+}
+
+export enum IMenuPosition {
+    LOGO= 'LOGO',
+    UPPER = 'UPPER',
+    MIDDLE = 'MIDDLE',
+    LOWER = 'LOWER',
+    BOTTOM = 'BOTTOM',
 }

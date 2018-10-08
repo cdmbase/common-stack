92e83a473eae81a141e36e952bacfe5d00e296d6
veeramarni <marni.srikanth@gmail.com>
2018-10-08 00:57:30 -0400

    update

--- packages-modules/cache-api/server/src/index.ts
+++ packages-modules/cache-api/server/src/index.ts
@@ -2,5 +2,6 @@ import module from './module';
 import { Feature } from '@common-stack/server-core';
 
 export * from './constants';
+export { Cache } from './services';
 export default new Feature(module);
 

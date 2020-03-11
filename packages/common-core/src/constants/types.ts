2c13d9a8cf7abd76cd28db2816ef67b41631b864
veeramarni <marni.srikanth@gmail.com>
2020-03-11 19:17:10 -0400

    update types

--- packages/common-core/src/constants/types.ts
+++ packages/common-core/src/constants/types.ts
@@ -3,6 +3,8 @@ export enum CommonType {
     LOGGER = 'LOGGER',
     ENVIRONMENT = 'ENVIRONMENT',
     HEMERA = 'HEMERA',
+    MOLECULER_BROKER = 'MOLECULER_BROKER',
+    PUBSUB = 'PUBSUB',
 }
 export enum TaggedType {
     MICROSERVICE = 'MICROSERVICE',

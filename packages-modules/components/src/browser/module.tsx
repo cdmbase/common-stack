a51b5070e92b44dd7c6dd5e980632db22ebf3a99
veeramarni <marni.srikanth@gmail.com>
2020-10-10 17:49:12 -0400

    update fix

--- packages-modules/components/src/browser/module.tsx
+++ packages-modules/components/src/browser/module.tsx
@@ -2,7 +2,4 @@ import * as  React from 'react';
 
 import {Feature} from '@common-stack/client-react';
 
-export default new Feature({
-  // @ts-ignore
-  routeConfig: [{}],
-});
+export default new Feature({});

1bd18240e15d27204c8fcc722a5aa29b0c4b3e58
Veera Marni <marni.srikanth@gmail.com>
2021-01-06 15:02:22 -0500

    added types

--- packages/common-client-react/src/interfaces/menu.ts
+++ packages/common-client-react/src/interfaces/menu.ts
@@ -25,8 +25,57 @@ export interface IMenuItem {
     priority?: number
 }
 
+export interface IMenuDataItem {
+    /**
+     * @name submenu
+     */
+    children?: IMenuDataItem[];
+    /**
+     * @name Hide child nodes in the menu
+     */
+    hideChildrenInMenu?: boolean;
+    /**
+     * @name hideSelf and children in menu
+     */
+    hideInMenu?: boolean;
+    /**
+     * @name Icon of the menu
+     */
+    icon?: React.ReactNode;
+    /**
+     * @name Internationalization key for custom menus
+     */
+    locale?: string | false;
+    /**
+     * @name The name of the menu
+     */
+    name?: string;
+    /**
+     * @name is used to calibrate the selected value, default is path
+     */
+    key?: string;
+    /**
+     * @name disable menu option
+     */
+    disabled?: boolean;
+    /**
+     * @name path
+     */
+    path?: string;
+    /**
+     * @name custom parent node
+     * @description When this node is selected, the node of parentKeys is also selected
+     */
+    parentKeys?: string[];
+    /**
+     * @name hides itself and elevates child nodes to its level
+     */
+    flatMenu?: boolean;
+    [key: string]: any;
+}
+
 export enum IMenuPosition {
-    LOGO= 'LOGO',
+    LOGO = 'LOGO',
     UPPER = 'UPPER',
     MIDDLE = 'MIDDLE',
     LOWER = 'LOWER',
